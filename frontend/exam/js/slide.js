c21a6c92e05fe53f5df37593093ebbce5974a42c
ministori-yonsei <74632170+ministori-yonsei@users.noreply.github.com>
2021-01-27 12:23:28 +0900

    Add files via upload

--- frontend/exam/js/slide.js
+++ frontend/exam/js/slide.js
@@ -3,16 +3,51 @@ $(function(){
     let currentIndex = 0;
     let nextIndex = 1;
 
-    $('.view-image').eq(0).css({left:0});
-    $('.view-image').eq(1).css({left:'-300px'});
-    $('.view-image').eq(2).css({left:'-300px'});
+    $('.image1').eq(0).css({left:0});
+    $('.image1').eq(1).css({left:'-300px'});
+    $('.image1').eq(2).css({left:'-300px'});
+
+    setInterval(function(){
+
+        $('.image1').eq(currentIndex).animate({
+            left:'-300px'
+        });
+    
+        $('.image1').eq(nextIndex).css({
+            left:'300px'
+        }).animate({
+            left:'0px'
+        });
+
+        currentIndex = nextIndex;
+        nextIndex++;
+
+        if(nextIndex > 2){
+            nextIndex = 0;
+        }
+
+    },3000);
 
 
-    $('.view-image').eq(currentIndex).animate({
-        left:'-300px'
-    });
+    let currentIndex2 = 0;
+    let nextIndex2 = 1;
 
-    $('.view-image').eq(nextIndex).animate();
+    setInterval(function(){
+
+        $('.image2').eq(currentIndex2).removeClass('jump in').addClass('out').delay(1000).queue(function(){
+            $(this).removeClass('out').addClass('jump').dequeue();
+        });
+    
+        $('.image2').eq(nextIndex2).removeClass('jump out').addClass('in');
+
+        currentIndex2 = nextIndex2;
+        nextIndex2++;
+
+        if(nextIndex2 > 2){
+            nextIndex2 = 0;
+        }
+
+    },3000);
 
 
 });
